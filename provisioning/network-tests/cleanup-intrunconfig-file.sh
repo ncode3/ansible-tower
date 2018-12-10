321d6635d07df9e1a826754f7dc35c557b41a991
David Taylor <dataylor@localhost.localdomain>
2018-12-10 10:58:11 -0500

    updating network tests

---
+++ provisioning/network-tests/cleanup-intrunconfig-file.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sed -i -e "s/    - '/    '/g" network-backups/intrunconfig_grotto.txt 
+sed -i -e "s/-   -/-  /g" network-backups/intrunconfig_grotto.txt 
+sed -i -e "s/    - e/    e/g" network-backups/intrunconfig_grotto.txt 
